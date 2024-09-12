98571722453f8d0c6759f05d98f672548c4d6733
Stefan Gogov <stefangogov@proton.me>
2024-09-12 20:20:47 +0300

    refactor: Update Astro config and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon"; // Ensure this package is still compatible
+import icon from "astro-icon";
 
 import { remarkReadingTime } from "./src/utils/all";
+import rehypePluginImageNativeLazyLoading from "rehype-plugin-image-native-lazy-loading";
 
 export default defineConfig({
   site: "https://codingly.netlify.app/",
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
+    rehypePlugins: [rehypePluginImageNativeLazyLoading],
     extendDefaultPlugins: true,
   },
   integrations: [tailwind(), mdx(), sitemap(), icon()],
